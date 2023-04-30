e49181b71e0f089eb76726a56cde7a4d03023fba
Masaki Hara <ackie.h.gmai@gmail.com>
2023-04-30 22:15:13 +0900

    Generate feed

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,22 @@ use axum::http::header::CONTENT_TYPE;
 use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::Router;
+use serde::Serialize;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
 async fn atom() -> impl IntoResponse {
-    ([(CONTENT_TYPE, "application/atom+xml")], "<>")
+    let feed = Feed {
+        xmlns: Feed::XMLNS,
+        title: Title {
+            type_: "text",
+            text: "タイトル".to_owned(),
+        },
+    };
+    let feed = quick_xml::se::to_string(&feed).unwrap();
+    ([(CONTENT_TYPE, "application/atom+xml")], feed)
 }
 
 #[shuttle_runtime::main]
@@ -18,4 +27,25 @@ async fn axum() -> shuttle_axum::ShuttleAxum {
         .route("/novels/:id/atom.xml", get(atom));
 
     Ok(router.into())
+}
+
+#[derive(Serialize)]
+#[serde(rename = "feed")]
+struct Feed {
+    #[serde(rename = "@xmlns")]
+    xmlns: &'static str,
+    title: Title,
+}
+
+#[derive(Serialize)]
+#[serde(rename = "title")]
+struct Title {
+    #[serde(rename = "@type")]
+    type_: &'static str,
+    #[serde(rename = "$text")]
+    text: String,
+}
+
+impl Feed {
+    const XMLNS: &str = "http://www.w3.org/2005/Atom";
 }
