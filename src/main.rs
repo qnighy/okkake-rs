09c38a7003a8338b4e4b327315b09de2c492a2c9
Masaki Hara <ackie.h.gmai@gmail.com>
2023-04-30 22:01:08 +0900

    Add endpoint

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,21 @@
-use axum::{routing::get, Router};
+use axum::http::header::CONTENT_TYPE;
+use axum::response::IntoResponse;
+use axum::routing::get;
+use axum::Router;
 
 async fn hello_world() -> &'static str {
     "Hello, world!"
 }
 
+async fn atom() -> impl IntoResponse {
+    ([(CONTENT_TYPE, "application/atom+xml")], "<>")
+}
+
 #[shuttle_runtime::main]
 async fn axum() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/hello", get(hello_world));
+    let router = Router::new()
+        .route("/hello", get(hello_world))
+        .route("/novels/:id/atom.xml", get(atom));
 
     Ok(router.into())
 }
